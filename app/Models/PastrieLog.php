b9c1ae32ac1626eabb2e4dcb1b56c1aeddecc618
DrUribe855 <yeisonl2036@gmail.com>
2025-09-15 15:23:55 -0500

    Add: Bakery view

--- app/Models/PastrieLog.php
+++ app/Models/PastrieLog.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PastrieLog extends Model
 {
+
+    protected $fillable = [
+        'establishment_id',
+        'user_id',
+        'image_url'
+    ];
+
     /* Relación al modelo de usuario */
     public function user() : BelongsTo {
         return $this->BelongsTo(User::class);
